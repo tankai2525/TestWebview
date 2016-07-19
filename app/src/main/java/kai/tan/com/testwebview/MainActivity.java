81ab3954bb65680bfa6e33b1df3f71cb5ce41df0
luobodidi <1067629251@qq.com>
2016-07-19 11:03:22 +0800

    first test

---
+++ app/src/main/java/kai/tan/com/testwebview/MainActivity.java
@@ -0,0 +1,45 @@
+package kai.tan.com.testwebview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView mWebView = (WebView) findViewById(R.id.webView);
+
+        mWebView.getSettings().setDomStorageEnabled(true);//处理天猫商品不显示问题aaa
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        //        mWebView.getSettings().setUseWideViewPort(true);//读取 "viewport"网页meta标签
+        //        mWebView.getSettings().setBuiltInZoomControls(true);
+        //        mWebView.getSettings().setSupportZoom(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("http") || url.startsWith("file:///android_asset")) {
+                    view.loadUrl(url);
+                } else {
+                    if (url.startsWith("mqqwpa:") || url.startsWith("tencent:")) {
+
+                    } else if (url.startsWith("taobao:")) {
+                        return true;
+                    } else if (url.startsWith("tmall:")) {//屏蔽天猫
+                        return true;
+                    } else {
+                        view.loadUrl(url);
+                    }
+
+                }
+                return true;
+            }
+        });
+        mWebView.loadUrl("https://m.taobao.com");
+    }
+
+
+}
