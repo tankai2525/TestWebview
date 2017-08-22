628b6b717878eea6a5f52d8b339ed8afa482f56b
luobodidi <1067629251@qq.com>
2017-08-22 18:24:34 +0800

    2017年8月22日18:24:23

--- app/src/main/java/kai/tan/com/testwebview/MainActivity.java
+++ app/src/main/java/kai/tan/com/testwebview/MainActivity.java
@@ -4,19 +4,26 @@ import android.annotation.SuppressLint;
 import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView img;
+    private Toast mToast;
 
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint({"SetJavaScriptEnabled", "AddJavascriptInterface"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -25,12 +32,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img = (ImageView)findViewById(R.id.img);
+        img = (ImageView) findViewById(R.id.img);
 
         final WebView mWebView = (WebView) findViewById(R.id.webView);
         assert mWebView != null;
 //        mWebView.getSettings().setDomStorageEnabled(true);//处理天猫商品不显示问题
-//        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.addJavascriptInterface(new JSObject(), "android");
 //        mWebView.setWebViewClient(new WebViewClient() {
 //            @Override
 //            public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -74,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 //        mWebView.loadUrl("https://api.qirexiaoshuo.com/my");
-        mWebView.loadUrl("https://api.qirexiaoshuo.com:8443/my");
+        mWebView.loadUrl("file:///android_asset/no-wifi.html");
 
         float denstity = this.getApplicationContext().getResources().getDisplayMetrics().density;
         int denstityDpi = this.getApplicationContext().getResources().getDisplayMetrics().densityDpi;
@@ -83,13 +91,41 @@ public class MainActivity extends AppCompatActivity {
         Logger.d("widthPixels:" + widthPixels + "-heightPixels:" + heightPixels);
         Logger.d("denstity:" + denstity);// A31u 1.5
         Logger.d("denstityDpi:" + denstityDpi);// A31u 240
+        Logger.d("60dpTopx:" + (int) (60 * denstity + 0.5));
 
-        Logger.d("60dpTopx:" + (int)(60 * denstity + 0.5));
+        try {
+            String json = null;
+            JSONObject jsonObject = new JSONObject(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    Toast toast;
+
+    public void onClick(View view) {
+        //主线程
+        Logger.d("Thread:" + Thread.currentThread().getName());
+        //这里会报android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
+        toast.setText("Thread:" + Thread.currentThread().getName());
+        toast.show();
+    }
+
+    public class JSObject {
+
+        @JavascriptInterface
+        public void toast() {
+            //Javabride线程
+            Logger.d("Thread:" + Thread.currentThread().getName());
+            toast = Toast.makeText(MainActivity.this, "Thread:" + Thread.currentThread().getName(), Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        Logger.d("imgWidth:" + img.getWidth() + "-imgHeight:"+ img.getHeight());
+        Logger.d("imgWidth:" + img.getWidth() + "-imgHeight:" + img.getHeight());
     }
 }
